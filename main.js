6b3e46d432782892d8d0a9cafaf5642d7723ef8e
Guntas2601 <97093375+Guntas2601@users.noreply.github.com>
2023-05-27 13:45:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,52 @@ Webcam.set({
     var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
     }
+    function check(){
+        img = document.getElementById("image1");
+        classifier.classify(img, gotResult);
+    }
+    
+    function gotResult(error, results) {
+
+        if(error) {
+            console.error(error);
+        } else{
+    
+    console.log(results);
+    document.getElementById("resultname1").innerHTML = results[0].label;
+    document.getElementById("resultname2").innerHTML = results[1].label;
+    prediction1 = results[0].label;
+    prediction2 = results[1].label;
+      
+    spek();
+    if(results[0].label== "Best")
+    {
+        document.getElementById("resultgesture1").innerHTML="👍"
+    }
+    if(results[0].label== "Victory")
+    {
+        document.getElementById("resultgesture1").innerHTML="✌"
+    }
+    if(results[0].label== "Amazing")
+    {
+        document.getElementById("resultgesture1").innerHTML="👌"
+    }
+   
+    
+    if(results[1].label== "Best")
+    {
+        document.getElementById("resultgesture2").innerHTML="👍"
+    }
+    if(results[1].label== "Victory")
+    {
+        document.getElementById("resultgesture2").innerHTML="✌"
+    }
+    if(results[1].label== "Amazing")
+    {
+        document.getElementById("resultgesture2").innerHTML="👌"
+    
+    
+    
+    }
+    }
+}
